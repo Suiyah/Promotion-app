e9ba3168a6ed7dc11afe3a61dfdfb739b057f4df
Suiyah <pent4con@gmail.com>
2024-11-21 13:38:25 +0500

    button

--- promotion/src/Prsn.js
+++ promotion/src/Prsn.js
@@ -19,6 +19,8 @@ const Person = () => {
       <p><strong>Age:</strong> {person.age}</p>
       <p><strong>Location:</strong> {person.location}</p>
       <p><strong>Hobbies:</strong> {person.hobbies.join(", ")}</p>
+
+      <button>Click Me</button>
     </div>
   );
 };
